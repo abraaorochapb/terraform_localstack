9a3a83c3a8845531cff5b56805e299674cae46d6
abraaorocha <abraaorochapb@gmail.com>
2025-05-20 06:45:06 -0300

    criação do arquivode  variaveis

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "access_key" {
+    description = "AWS Access Key"
+    type        = string
+    default = "minhachave"
+}
+
+variable "secret_key" {
+    description = "AWS Secret Key"
+    type        = string
+    default = "minhasenha"
+}
+
+variable "aws_region" {
+    description = "AWS Region"
+    type        = string
+    default = "us-east-1"
+}
